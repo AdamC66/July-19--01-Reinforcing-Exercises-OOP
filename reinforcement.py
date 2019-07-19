9156ca079d93f1f47761bbb5da3cc67777630cef
adamc66 <adam.cote66@gmail.com>
2019-07-19 13:36:40 -0400

    reinforecement exercise complete

--- reinforcement.py
+++ reinforcement.py
@@ -9,3 +9,27 @@
 # Create a TodoList class with a tasks list (which will contain instances of the Task class). 
 # Create an __init__ method and an add_task method that allows you to pass in an instance of your Task class. 
 # Try creating a todo list and adding your three tasks to it.
+
+class Task():
+    def __init__(self, description, due_date):
+        self.description = description
+        self.due_date = due_date
+
+class To_Do_List():
+    def __init__(self):
+        self.tasklist=[]
+
+    def add_task(self,description, due_date):
+        self.tasklist.append(Task(description,due_date))
+
+    def displaylist(self):
+        for task in self.tasklist:
+            print(task.description,"-", task.due_date)    
+
+adams_list = To_Do_List()
+
+adams_list.add_task("Get Money", "Tomorrow")
+adams_list.add_task("Bro down", "Next week")
+adams_list.add_task("Sell out", "Never")
+
+adams_list.displaylist()
